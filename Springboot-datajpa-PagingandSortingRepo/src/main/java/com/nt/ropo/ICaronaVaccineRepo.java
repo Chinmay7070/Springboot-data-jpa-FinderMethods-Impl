2b838c63792f6b4dc3acdeb85eda5e2da946c6c9
Chinmay Chaudhari <chaudharichinmay227@gmail.com>
2025-08-15 12:52:37 +0530

    Delete Springboot-datajpa-PagingandSortingRepo directory

--- Springboot-datajpa-PagingandSortingRepo/src/main/java/com/nt/ropo/ICaronaVaccineRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nt.ropo;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import com.nt.Entity.CoronaVaccine;
-
-public interface ICaronaVaccineRepo extends PagingAndSortingRepository<CoronaVaccine, Long> {
-
-}
